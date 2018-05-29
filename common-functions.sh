0e2ed45048dbb128aef2eba40485076d95a04d05
Pieter (P.H.C.) Vallen <Pieter.Vallen@ing.com>
2018-05-30 00:05:11 +0200

    Updated for use with Oracle JDK10

--- common-functions.sh
+++ common-functions.sh
@@ -10,8 +10,8 @@ esc=$(echo -en "\033")
 info="${esc}[0;33m"
 normal=$(echo -en "${esc}[m\017")
 
-java -version 2>&1 | grep -q '"9' || (echo "**** Fix your PATH! Cannot find command 'java' (version 9) ****" && java -version && exit 1)
-javac -version 2>&1 | grep -q '9' || (echo "**** Fix your PATH! Cannot find command 'javac' (version 9) ****" && javac -version && exit 1)
+java -version 2>&1 | grep -q '"10' || (echo "**** Fix your PATH! Cannot find command 'java' (version 10) ****" && java -version && exit 1)
+javac -version 2>&1 | grep -q '10' || (echo "**** Fix your PATH! Cannot find command 'javac' (version 10) ****" && javac -version && exit 1)
 
 runTree()
 {
